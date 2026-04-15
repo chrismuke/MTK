fba277b644b9d072c090a550892e811fbd0756e9
chrism <chrissem@gmail.com>
2026-04-15 13:25:07 +0200

    fix: use git URL for DICOM-Decoder dependency instead of local path

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.19"),
-        .package(path: "../DICOM-Decoder"),
+        .package(url: "https://github.com/ThalesMMS/DICOM-Decoder.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
     ],
     targets: [
